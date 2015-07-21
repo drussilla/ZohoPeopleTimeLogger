8ddc1627bf8593134e26fcbaf98be827a0069dad
Ivan Derevianko <drussilla7@gmail.com>
2015-07-21 23:06:30 +0200

    Login dialog added

--- ZohoPeopleTimeLogger/MainWindow.xaml.cs
+++ ZohoPeopleTimeLogger/MainWindow.xaml.cs
@@ -1,28 +1,68 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using ZohoPeopleClient;
+using MahApps.Metro.Controls.Dialogs;
+using ZohoPeopleClient.Exceptions;
 
 namespace ZohoPeopleTimeLogger
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private async void ShowLoginDialog()
+        {
+            LoginDialogData result;
+            do
+            {
+                result =
+                    await
+                        this.ShowLoginAsync("Authentication", "Enter your credentials",
+                            new LoginDialogSettings
+                            {
+                                AnimateHide = false,
+                                AnimateShow = true
+                            });
+            } while (result == null);
+
+            ProgressDialogController progress = await this.ShowProgressAsync("Authentication", "Wait for response from server", false);
+            progress.SetIndeterminate();
+
+            bool isLoginError = false;
+            string errorText = "";
+            var zohoClient = new ZohoClient();
+            try
+            {
+                await zohoClient.LoginAsync(result.Username, result.Password);
+            }
+            catch (ApiLoginErrorException exception)
+            {
+                errorText = exception.Response;
+                isLoginError = true;
+            }
+            
+            await progress.CloseAsync();
+
+            if (isLoginError)
+            {
+                await this.ShowMessageAsync("Error durong login", errorText);
+            }
+            else
+            {
+                await this.ShowMessageAsync("Authentication Information",
+                            String.Format("Username: {0}\nPassword: {1}", result.Username, result.Password));
+            }
+        }
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            ShowLoginDialog();
         }
     }
 }
---
+++ ZohoPeopleTimeLogger/ViewModel/LoginViewModel.cs
@@ -0,0 +1,10 @@
+﻿using GalaSoft.MvvmLight;
+
+namespace ZohoPeopleTimeLogger.ViewModel
+{
+    public class LoginViewModel : ViewModelBase
+    {
+        public string Login { get; set; }
+        public string Password { get; set; }
+    }
+}
