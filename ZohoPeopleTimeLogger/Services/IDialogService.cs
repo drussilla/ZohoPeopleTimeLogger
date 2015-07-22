67cc3fead03241ee6ce35c5588081968c716dfac
Ivan Derevianko <drussilla7@gmail.com>
2015-07-22 23:21:42 +0200

    IDialogService added

---
+++ ZohoPeopleTimeLogger/Services/IDialogService.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using MahApps.Metro.Controls.Dialogs;
+
+namespace ZohoPeopleTimeLogger.Services
+{
+    public interface IDialogService
+    {
+        Task<LoginDialogData> ShowLogin();
+    }
+}
