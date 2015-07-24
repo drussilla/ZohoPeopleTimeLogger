9760c9e5b0fd1a740406fd41285d2824b479702e
Ivan Derevianko <drussilla7@gmail.com>
2015-07-24 22:40:43 +0200

    Missing files were added

---
+++ ZohoPeopleTimeLogger.UnitTests/ViewModels/MainWindowViewModelTests.cs
@@ -0,0 +1,36 @@
+﻿using System.Threading.Tasks;
+using Moq;
+using Ploeh.AutoFixture.Xunit2;
+using Xunit;
+using ZohoPeopleClient;
+using ZohoPeopleTimeLogger.Controllers;
+using ZohoPeopleTimeLogger.Model;
+using ZohoPeopleTimeLogger.Services;
+using ZohoPeopleTimeLogger.ViewModel;
+
+namespace ZohoPeopleTimeLogger.UnitTests
+{
+    public class MainWindowViewModelTests
+    {
+        [Theory, AutoMoqData]
+        public void ViewReady_NoLoginInformationStored_AskForLoginAndSaveValidData(
+            [Frozen]Mock<IAuthenticationStorage> auth,
+            [Frozen]Mock<IDialogService> dialog,
+            [Frozen]Mock<IZohoClient> zoho,
+            [Frozen]Mock<ILoginController> login,
+            AuthenticationData data,
+            MainWindowViewModel target)
+        {
+            auth.Setup(x => x.GetAuthenticationData()).Returns((AuthenticationData)null);
+            login.Setup(x => x.Login()).Returns(Task.Run(() => data));
+
+            target.ViewReady();
+
+            Assert.True(target.IsLoggedIn);
+            Assert.Equal(data.UserName, target.UserName);
+
+            login.Verify(x => x.Login(), Times.Once);
+            auth.Verify(x => x.SaveAuthenticationData(data));
+        }
+    }
+}
---
+++ ZohoPeopleTimeLogger/Controllers/LoginController.cs
@@ -0,0 +1,58 @@
+﻿using System.Threading.Tasks;
+using ZohoPeopleClient;
+using ZohoPeopleClient.Exceptions;
+using ZohoPeopleTimeLogger.Model;
+using ZohoPeopleTimeLogger.Services;
+
+namespace ZohoPeopleTimeLogger.Controllers
+{
+    public class LoginController : ILoginController
+    {
+        private readonly IDialogService dialogService;
+
+        private readonly IZohoClient zohoClient;
+
+        public LoginController(IZohoClient zohoClient, IDialogService dialogService)
+        {
+            this.zohoClient = zohoClient;
+            this.dialogService = dialogService;
+        }
+
+        public async Task<AuthenticationData> Login()
+        {
+            var loginDetails = await dialogService.ShowLogin();
+
+            // cancel was pressed
+            if (loginDetails == null)
+            {
+                return null;
+            }
+
+            var progress = await dialogService.ShowProgress("Authentication", "Wait for response from server");
+            progress.SetIndeterminate();
+
+            var isError = false;
+            var errorMessage = "";
+            var token = "";
+            try
+            {
+                token = await zohoClient.LoginAsync(loginDetails.Username, loginDetails.Password);
+            }
+            catch (ApiLoginErrorException exception)
+            {
+                isError = true;
+                errorMessage = exception.Response;
+            }
+
+            await progress.CloseAsync();
+
+            if (isError)
+            {
+                await dialogService.ShowMessageAsync("Authentication error", errorMessage);
+                return null;
+            }
+
+            return new AuthenticationData {UserName = loginDetails.Username, Token = token};
+        }
+    }
+}
