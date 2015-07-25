9d9a27af00ff92100b37ebc435e466c8175ad991
Ivan Derevianko <drussilla7@gmail.com>
2015-07-25 23:20:32 +0200

    Missing files added

---
+++ ZohoPeopleTimeLogger/Services/IDaysService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using ZohoPeopleTimeLogger.ViewModel;
+
+namespace ZohoPeopleTimeLogger.Services
+{
+    public interface IDaysService
+    {
+        List<DayViewModel> GetDaysAsync(DateTime month);
+    }
+}
