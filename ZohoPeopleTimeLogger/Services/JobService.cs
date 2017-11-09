c73133a11ae7c9fb86cd34e3600ae134475fd550
Ivan Derevianko <ivan.derevyanko@novility.com>
2017-11-09 15:38:20 +0100

    select job based on assigned name

--- ZohoPeopleTimeLogger/Services/JobService.cs
+++ ZohoPeopleTimeLogger/Services/JobService.cs
@@ -9,14 +9,17 @@ namespace ZohoPeopleTimeLogger.Services
     public class JobService : IJobService
     {
         private readonly IZohoClient zohoClient;
+        private readonly IAuthenticationStorage authenticationStorage;
 
-        public JobService(IZohoClient zohoClient)
+        public JobService(IZohoClient zohoClient, IAuthenticationStorage authenticationStorage)
         {
             this.zohoClient = zohoClient;
+            this.authenticationStorage = authenticationStorage;
         }
 
         public async Task<string> GetJob(DateTime date)
         {
+            var authData = authenticationStorage.GetAuthenticationData();
             var allJobs = await zohoClient.TimeTracker.Jobs.GetAsync();
             var jobsForThisDay = allJobs
                 .Where(
@@ -30,7 +33,24 @@ namespace ZohoPeopleTimeLogger.Services
                 throw new JobNotFoundException(date);
             }
 
-            return jobsForThisDay.OrderByDescending(x => x.ToDate).First().JobId;
+            var fullName = await GetFullName(authData.UserName);
+
+            return jobsForThisDay.OrderByDescending(x => x.ToDate).First(x => x.Assignees.Any(y => y.Name == fullName)).JobId;
+        }
+
+        public async Task<string> GetFullName(string username)
+        {
+            var employees = await zohoClient.FetchRecord.GetByViewAsync("P_EmployeeView");
+
+            var employeeRecord =
+                employees.FirstOrDefault(
+                    x => ((string)x["Email ID"]).Equals(username, StringComparison.OrdinalIgnoreCase));
+            if (employeeRecord == null)
+            {
+                throw new Exception("Cannot find user Id. User name: " + username);
+            }
+
+            return employeeRecord["Last Name"] + " " + employeeRecord["First Name"];
         }
     }
 }
